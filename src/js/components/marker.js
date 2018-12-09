10efeec3e977ccb54ada977faaf5b9c47a62c8bd
ankush <saini.ankush29@gmail.com>
2018-12-09 13:47:22 +0530

    updates

--- src/js/components/marker.js
+++ src/js/components/marker.js
@@ -15,7 +15,7 @@ export default class Marker extends Component {
   render() {
     return (
       <div className={'popup'} onClick={this.showPopUp}>
-        <img src='map-marker.svg' style={{'width':'35px'}}/>
+        <img src='map-marker.svg' style={{'width':'35px'}} alt="map marker"/>
         <span className={"popuptext "+this.props.activePop}>{`${this.props.text} - Longitude: ${this.props.lng} and Latitude: ${this.props.lat}`}</span>
         {/*<span style={{'color':'#A9A9A9'}}>{this.props.text}</span>*/}
       </div>
